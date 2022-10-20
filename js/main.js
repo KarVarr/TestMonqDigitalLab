fc18acfb689eab20c6bbeba012046a6e5211d24d
Karen Vardanian <karenvardanian@MacBook-Air-Karen.local>
2022-10-20 22:21:38 +0300

    added the switcher

--- js/main.js
+++ js/main.js
@@ -4,5 +4,20 @@
 const year = (document.querySelector('#curYear').textContent =
   new Date().getFullYear());
 
+//theme
+
+const button = document.querySelector('.button--submit');
+
+button.addEventListener('click', e => {
+  e.preventDefault();
+  let selected = document.getElementsByName('theme');
+  for (const color of selected) {
+    if (color.checked) {
+      document
+        .querySelector('[title="theme"]')
+        .setAttribute('href', `css/theme-${color.value}.css`);
+    }
+  }
 
 
+});
